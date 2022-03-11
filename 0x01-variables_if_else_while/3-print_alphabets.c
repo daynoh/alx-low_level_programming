e68ffb5f69ace12d715c7c176e26d108da3e35c8
Musingila <justdaynoh@gmail.com>
2022-03-11 18:44:34 +0300

    Update 3-print_alphabets.c
    working

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
+
 /**
- * main - dfadslfa
+ * main - Prints the alphabet in lowercase, and then in uppercase.
  *
- * Return: is zero
+ * Return: Always 0.
  */
 int main(void)
 {
-	char c;
-	
-	for (c = 'a'; c <= 'z'; ++c)
-		putchar(c);
-	for (c = 'A'; c <= 'Z'; ++c)
-		putchar(c);
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+
 	putchar('\n');
+
 	return (0);
 }
