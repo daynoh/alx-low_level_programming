e4eb2f746d9b325b293bd67a984893bbf29ecef1
daynoh <justdaynoh@gmail.com>
2022-03-11 01:40:58 -0800

    hellow world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
+/* A function that just prints something */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
