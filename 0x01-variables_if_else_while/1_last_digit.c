a08a71319adb029fc898493e744011e83f38d425
Musingila <justdaynoh@gmail.com>
2022-03-11 18:26:10 +0300

    Create 1_last_digit.c
    this should work

---
+++ 0x01-variables_if_else_while/1_last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - nothing
+ *
+ * Return: always zero
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	L = n % 10;
+
+	if (L > 5)
+		printf("%s %d is %d and is greater than 5\n", str, n, L);
+	else if (L == 0)
+		printf("%s %d is %d and is 0\n", str, n, L);
+	else if (L < 6)
+		printf("%s %d is %d and is less than 6 and not 0\n", str, n, L);
+	
+	return (0);
+}
