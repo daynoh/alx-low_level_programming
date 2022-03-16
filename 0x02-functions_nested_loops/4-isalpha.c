69b46329de519a3aa22c3f85e1eb09cb889d9c64
Moose <justdaynoh@gmail.com>
2022-03-16 04:50:32 +0300

    alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isalpha- checks whether or not a value is uppercase
+ * @c: ascii value of character
+ *
+ * Return: 1 if true 0 if false
+ */
+
+int _isalpha(int c)
+{
+	return (c >= 65 && c <= 90);
+}
