8b1b35ff7b03cacd7c2fdef7a62faeb9c40d5dd1
Moose <justdaynoh@gmail.com>
2022-03-16 05:53:08 +0300

    jack

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,17 +8,20 @@
 
 void jack_bauer(void)
 {
-	int i;
-	int j;
+	int i = 0;
 
 	while (i <= 23)
 	{
+		int j = 0;
+
 		while(j <= 59)
 		{
 			_putchar(i + '0');
 			_putchar(':');
 			_putchar(j + '0');
+			j++;
 		}
 		_putchar('\n');
+		i++;
 	}
 }
