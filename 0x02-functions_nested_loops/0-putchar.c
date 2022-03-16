e2d799f848cfdcf6d31b3476bb0c6f7f811ce78b
Moose <justdaynoh@gmail.com>
2022-03-16 03:18:18 +0300

    Print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+/**
+ * main - Entry to program
+ *
+ * Return: returns 0 if successful
+ */
+
+int main(void)
+{
+	char name[] = "_putchar";
+	int i = 0;
+	
+	while (name[i] != '\0')
+	{
+		_putchar(name[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
