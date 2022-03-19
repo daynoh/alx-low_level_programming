14bace8751db3b2974cb8531167418fd7636621b
Moose <justdaynoh@gmail.com>
2022-03-19 05:53:12 +0300

    update

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -24,6 +24,18 @@ int largest_number(int a, int b, int c)
     {
         largest = c;
     }
+    else if (a > b && b == c)
+    {
+        largest = a;
+    }
+    else if (b > a && a == c)
+    {
+        largest = b;
+    }
+    else if (c > a && a == b)
+    {
+        largest = c;
+    }
 
     return (largest);
 }
