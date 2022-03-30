a6e0ec784a98876cc283e277fc601b79094dc70b
Moose <justdaynoh@gmail.com>
2022-03-30 11:54:21 +0300

    something

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+ * puts- prints values in an char array skiping one  value
+ *
+ * Return : always 0.
+ */
+
+void puts(char *str);
+{
+	for(int i = 0; str[i] != "\n"; i+2)
+	{
+		_putchar(str[i])
+	}
+}
