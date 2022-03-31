d9d5199fb65d651d4be0ce12f5e8da3012ce83db
Moose <justdaynoh@gmail.com>
2022-03-31 04:34:37 +0300

    reverse string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - reverses string
+ * @s : character
+ *
+ * Return : always 0
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (s != '\n')
+	{
+		_print_rev_recursion(s+1);
+		_putchar(s);
+	}
+	_putchar('\n');
+}
