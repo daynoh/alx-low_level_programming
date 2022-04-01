43820a79f526f259d2fa959829afb9d1da4566d8
Moose <justdaynoh@gmail.com>
2022-04-01 16:03:27 +0300

    my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - returns name of program
+ * @argc: count of parameters 
+ * @argv: array of parameters
+ *
+ * Return : 1 if success
+ */
+
+int main(int argc, char *argv)
+{
+	printf("%s",*argv);
+	return (1);
+}
